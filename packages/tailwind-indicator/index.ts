19bedffb9b18120725a1a9ac150e82170f42c49e
Jordi <1439075+jllahi@users.noreply.github.com>
2023-12-08 01:42:40 +0100

    Add package tailwind-indicator

---
+++ packages/tailwind-indicator/index.ts
@@ -0,0 +1,6 @@
+// Do not write code directly here, instead use the `src` folder!
+// Then, use this file to export everything you want your user to access.
+import "tailwindcss/tailwind.css"
+import TailwindIndicator from "./src/TailwindIndicator.astro"
+
+export default TailwindIndicator
